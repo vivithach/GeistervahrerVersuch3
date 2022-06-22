8b82b5fad5eb1ce45705b32f4ffb1feabbda76ea
basti <sebastian@giesbert-online.de>
2022-06-22 15:53:42 +0200

    Lebensbalken Testklasse hinzugefügt

--- src/Lebensbalken.java
+++ src/Lebensbalken.java
@@ -1,33 +1,40 @@
 import processing.core.PApplet;
 
-public class Lebensbalken extends Spielelement{
+public class Lebensbalken extends Spielelement {
     private int Leben;
-    Lebensbalken(PApplet app){
+
+    Lebensbalken(PApplet app) {
         super(app);
         setPosX(20);
         setPosY(20);
         Leben = 250;
     }
 
-    public void minusLeben(){
-        Leben -=1;
+    public void minusLeben() {
+        Leben -= 1;
     }
 
-    public void plusLeben(){
-        if(Leben < 250)
-            Leben +=50;
-        if(Leben > 250){
+    public void plusLeben() {
+        if (Leben < 250)
+            Leben += 50;
+        if (Leben > 250) {
             Leben = 250;
         }
     }
 
-    public void drawing(){
+    public void drawing() {
         app.stroke(255);
         app.strokeWeight(10);
-        app.line(getPosX(), getPosY(), Leben,getPosY());
+        app.line(getPosX(), getPosY(), Leben, getPosY());
     }
 
     public int getLeben() {
         return Leben;
     }
+
+    //nur für Testmethode benötigt
+    public void setLeben(int leben) {
+        Leben = leben;
+    }
+
 }
---
+++ test/LebensbalkenTest.java
@@ -0,0 +1,27 @@
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+import processing.core.PApplet;
+
+import static org.junit.Assert.*;
+
+public class LebensbalkenTest extends TestCase {
+
+    PApplet app;
+
+    @Test
+    public void testMinusLeben() {
+        Lebensbalken l = new Lebensbalken(app);
+        l.minusLeben();
+        Assert.assertEquals(249, l.getLeben());
+    }
+
+    @Test
+    public void testPlusLeben() {
+        Lebensbalken l = new Lebensbalken(app);
+        l.setLeben(180);
+        l.plusLeben();
+        Assert.assertEquals(230, l.getLeben());
+
+    }
+}
